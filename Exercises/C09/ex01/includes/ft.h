7aba4ba41278ef44eb940ae53ed0ea13824d05b8
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2021-07-15 08:58:13 +0000

    List C09 exercises.

---
+++ Exercises/C09/ex01/includes/ft.h
@@ -0,0 +1,5 @@
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+void	ft_putchar(int c);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
---
+++ Exercises/C09/ex02/ft_split.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+char **ft_split(char *str, char *charset)
+{
+	int	i;
+	int	j;
+	int *size;
+
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (charset[j])
+		{
+
+		}
+	}
+}
